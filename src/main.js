21af2f6eaebdf9ede35f09bd42776765e3785ccb
Dmseong <2with13@naver.com>
2024-05-17 17:59:06 +0900

    h

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
+function isValidEmail(email) {
+    let emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailPattern.test(email);
+}
+
 function validateForm() {
     let email = document.forms["lgn_form"]["email"].value;
     let password = document.forms["lgn_form"]["pswd"].value;
     if (email === "") {
         alert("아이디를 입력해 주세요");
+        return false;
+    }
+    else if (!isValidEmail(email)){
+        alert("이메일 형식이 바르지 못합니다");
         return false;
     }
     if (password === "") {
